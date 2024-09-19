8223ce4e20a45dd337e0ba38e3eb93a2b8691aac
chophandog <chophandog@gmail.com>
2024-09-19 14:06:34 +0700

    update bank default

--- app/_components/qr.tsx
+++ app/_components/qr.tsx
@@ -4,10 +4,48 @@ import React, { useEffect, useState } from "react";
 import { getBanks } from "./api";
 import Image from "next/image";
 
+const MY_BANKS = [
+	{
+		id: 1,
+		bin: "970422",
+		accountNumber: "0333829762",
+		accountName: "PHAN THANH TU",
+		shortName: "MBBank",
+	},
+	{
+		id: 2,
+		bin: "970403",
+		accountNumber: "060286834895",
+		accountName: "PHAN THANH TU",
+		shortName: "Sacombank",
+	},
+	{
+		id: 3,
+		bin: "970407",
+		accountNumber: "19072590042015",
+		accountName: "PHAN THANH TU",
+		shortName: "Techcombank",
+	},
+	{
+		id: 4,
+		bin: "970423",
+		accountNumber: "00006480830",
+		accountName: "PHAN THANH TU",
+		shortName: "TPBank",
+	},
+	{
+		id: 0,
+		shortName: "None",
+		accountNumber: "Tùy chỉnh",
+	},
+];
+
 const QR = () => {
 	const [banks, setBanks] = useState([]);
-	const [ip_bankName, setBankName] = useState("");
-	const [ip_accountNumber, setAccountNumber] = useState("");
+	const [ip_id, setId] = useState(1);
+	const [ip_bankBin, setBankBin] = useState(MY_BANKS[0].bin);
+	const [ip_accountNumber, setAccountNumber] = useState(MY_BANKS[0].accountNumber);
+	const [ip_accountName, setAccountName] = useState(MY_BANKS[0].accountName);
 	const [ip_amount, setAmount] = useState("");
 	const [ip_description, setDescription] = useState("");
 	const [img, setImg] = useState("");
@@ -17,7 +55,6 @@ const QR = () => {
 		getBanks().then((data) => {
 			if (data.code === "00") {
 				setBanks(data.data);
-				setBankName(data.data[0].bin);
 			}
 		});
 	}, []);
@@ -28,24 +65,40 @@ const QR = () => {
 		} else {
 			const amount = ip_amount ? `amount=${ip_amount}` : "";
 			const description = ip_description ? `addInfo=${ip_description}` : "";
-			setImg(`https://img.vietqr.io/image/${ip_bankName}-${ip_accountNumber}-print.jpg?${amount}&${description}`);
+			const name = ip_accountName ? `accountName=${ip_accountName}` : "";
+			setImg(
+				`https://img.vietqr.io/image/${ip_bankBin}-${ip_accountNumber}-print.jpg?${amount}&${description}&${name}`
+			);
 		}
 	};
 
-	const onDefault = () => {
-		setBankName("970422");
-		setAccountNumber("0333829762");
+	const handleBankBinChange = (e: any) => {
+		setBankBin(e.target.value);
+		setId(0);
+		setAccountNumber("");
+		setAccountName("");
 	};
 
-	const handleBankNameChange = (e: any) => {
-		setBankName(e.target.value);
+	const handleBankOptionChange = (e: any) => {
+		setId(+e.target.value);
+		if (+e.target.value !== 0) {
+			const bank = MY_BANKS.find((b) => b.id === +e.target.value);
+			setBankBin(bank?.bin || "");
+			setAccountName(bank?.accountName || "");
+			setAccountNumber(bank?.accountNumber || "");
+		} else {
+			setAccountName("");
+			setAccountNumber("");
+		}
 	};
 
 	const handleAccountNumberChange = (e: any) => {
 		if (error) {
 			setError(false);
 		}
+		setId(0);
 		setAccountNumber(e.target.value);
+		setAccountName("");
 	};
 
 	const handleAmountChange = (e: any) => {
@@ -59,7 +112,16 @@ const QR = () => {
 	return (
 		<main className="flex flex-col gap-4 justify-center">
 			<div className="custom-select">
-				<select onChange={handleBankNameChange} className="appearance-none" value={ip_bankName}>
+				<select onChange={handleBankOptionChange} className="appearance-none" value={ip_id}>
+					{MY_BANKS.map((bank) => (
+						<option key={bank.id} value={bank.id}>
+							{bank.shortName} - {bank.accountNumber}
+						</option>
+					))}
+				</select>
+			</div>
+			<div className="custom-select">
+				<select onChange={handleBankBinChange} className="appearance-none" value={ip_bankBin}>
 					{banks?.map((bank: any) => (
 						<option key={bank.bin} value={bank.bin}>
 							{bank.shortName} - {bank.name}
@@ -77,9 +139,6 @@ const QR = () => {
 			/>
 			<input type="number" placeholder="Số tiền" value={ip_amount} onChange={handleAmountChange} />
 			<input type="text" placeholder="Lời nhắn" value={ip_description} onChange={handleDescriptionChange} />
-			<button className="default" onClick={onDefault}>
-				MB - 0333829762
-			</button>
 			<button onClick={onGenerate}>Tạo mã</button>
 			{img && (
 				<div className="flex justify-center">
